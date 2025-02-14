42808bd0ce3840682cbd46c48dd2a3482e14a848
Joshua Duplaa <joshuaduplaa@outlook.com>
2025-02-14 17:21:19 -0600

    removed main branch files

--- api/Medical-Information.API/Medical-Information.API/Models/DTO/AddAdminReportDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Medical_Information.API.Models.DTO
-{
-    public class AddAdminReportDTO
-    {
-        public Guid AdminID { get; set; }
-        public Guid AdminQCLotID { get; set; }
-        public DateTime? CreatedDate { get; set; }
-    }
-}
